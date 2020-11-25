b11ac8ab991acffcd9661ff9ad482e8c2dd58896
Manish Choudhary <codespeech@pm.me>
2020-11-25 08:15:39 +0530

    Fixing page breaks

--- src/containers/Admin/Admin.js
+++ src/containers/Admin/Admin.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
-import { NavLink, Route, Switch } from 'react-router-dom';
-import Header from '../../components/Header';
-import Spinner from '../../utilComponents/Spinner/Spinner';
-import CategoryCreator from '../Admin/CategoryCreator';
-import QuestionCreator from './QuestionCreator';
+import React, { Component } from "react";
+import { NavLink, Route, Switch } from "react-router-dom";
+import Header from "../../components/Header";
+import Spinner from "../../utilComponents/Spinner/Spinner";
+//import CategoryCreator from "../Admin/CategoryCreator";
+//import QuestionCreator from "./QuestionCreator";
 
 class Admin extends Component {
-    render() {
-        return (
-            <>
-                <Header/>
-                <Switch>
-                    <Route path="/admin/home/category">
-                        <CategoryCreator />
-                    </Route>
-                    <Route path="/admin/home/questions">
-                        <QuestionCreator />
-                    </Route>
-                </Switch>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Header />
+        <Switch>
+          <Route path="/admin/home/category">{/*<CategoryCreator />*/}</Route>
+          <Route path="/admin/home/questions">{/*<QuestionCreator />*/}</Route>
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default Admin;
